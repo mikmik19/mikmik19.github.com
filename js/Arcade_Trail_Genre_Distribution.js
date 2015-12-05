5841858ebe67d6e777dac612c24d8fd4dab6aba2
Mikkel <mikkelheart@gmail.com>
2015-12-05 16:07:20 +0100

    Fixing the genre axis

--- js/Arcade_Trail_Genre_Distribution.js
+++ js/Arcade_Trail_Genre_Distribution.js
@@ -2,7 +2,7 @@
     var svg = d3.select('#Arcade_Trail_Genre_Distribution');
 
     var margin = {
-        left: 30,
+        left: 50,
         top: 30,
         right: 30,
         bottom: 30,
@@ -13,8 +13,6 @@
     var innerWidth = outerWidth - margin.left - margin.right;
     var innerHeight = outerHeight - margin.top - margin.bottom;
 
-    // The name of the columns from the CSV file that should be used
-    // for the x and y coordinates respectively and the series.
     var ID = 'ID';
     var Num_games = 'Num_Games';
 
@@ -27,16 +25,24 @@
     var yAxisG = g.append('g')
         .attr('class', 'axis');
 
-    var xScale = d3.scale.linear().range([innerWidth, 0]);
-    var yScale = d3.scale.linear().range([0, innerHeight]);
-
-    var xAxis = d3.svg.axis().scale(xScale).orient('left');
-    var yAxis = d3.svg.axis().scale(yScale).orient('top');
+    var xScale = d3.scale.linear()
+        .range([0,innerWidth]);
     
+    var yScale = d3.scale.linear()
+        .range([0, innerHeight]);
 
+    var xAxis = d3.svg.axis()
+        .scale(xScale)
+        .orient('top');
+    
+    var yAxis = d3.svg.axis()
+        .scale(yScale)
+        .ticks([32])
+        .orient('left');
+    
     function render(data) {
         xScale.domain(d3.extent(data, function(d) { return d[Num_games]; }));
-        yScale.domain(d3.extent(data, function(d) { return d[ID]; }));
+        yScale.domain([d3.min(data, function(d) { return d[ID]; })-0.5, d3.max(data, function(d) { return d[ID]; })+0.5]);
 
         xAxisG.call(xAxis);
         yAxisG.call(yAxis);
@@ -45,10 +51,11 @@
 
         bar.enter().append('rect')
             .attr('x', 0)
-            .attr('y', function(d) { return yScale(d[ID]); })
+            .attr('y', function(d) { return yScale(d[ID]) - 5; })
             .attr('height', 12)
-            .attr('width', function(d) { return innerWidth - xScale(d[Num_games]); })
-            .attr('fill', 'orange' );
+            .attr('width', function(d) { return xScale(d[Num_games]); })
+            .attr('fill', 'orange' )
+            .attr('padding',2);
 
         bar.exit().remove();
     }
@@ -56,6 +63,7 @@
     function parse(d) {
         d[Num_games] = parseFloat(d[Num_games]);
         d[ID] = parseFloat(d[ID]);
+
         return d;
     }
 
