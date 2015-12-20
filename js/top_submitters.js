7147ccbc72155cf7929cf9f961dfaa7a6c16ef25
Mikkel <mikkelheart@gmail.com>
2015-12-20 12:50:22 +0100

    Finished the top submitters graph

--- js/top_submitters.js
+++ js/top_submitters.js
@@ -1,13 +1,13 @@
 (function(){
 	var svg = d3.select('#top_submitters');
 
-	/* loading and parsing the data */
+	/* loading and parsing the d */
 	d3.csv('/data/top_submitters.csv', parse, render);
 
-	function parse(d){
-		d[num_submits] = parseFloat(d[num_submits]);
-		d[id] = parseFloat(d[id]);
-		return d;
+	function parse(data){
+		data[num_submits] = parseFloat(data[num_submits]);
+		data[id] = parseFloat(data[id]);
+		return data;
 	}
 	
 	var num_submits = 'num_submits';
@@ -27,12 +27,12 @@
 	var innerWidth = outerWidth - margin.left - margin.right;
 	var innerHeight = outerHeight - margin.top - margin.bottom;
 
-	var graph = svg.append('graph')
+	var g = svg.append('g')
 		.attr('transform','translate(' + margin.left + ', ' + margin.top + ')');
 
-	var xAxisG = graph.append('graph')
+	var xAxisG = g.append('g')
 		.attr('class','axis')
-	var yAxisG = graph.append('graph')
+	var yAxisG = g.append('g')
 		.attr('class','axis')
 
 	var xScale = d3.scale.linear()
@@ -50,27 +50,40 @@
 		.ticks([0])
 		.orient('left');
 
-	/* rendering the data */
-	function render(d){
+	/* adding the x label */
+	svg.append('text')
+		.attr('x',margin.left + innerWidth/2)
+		.attr('y',20)
+		.style('text-anchor','middle')
+		.text('Submissions');
+
+	/* rendering the d */
+	function render(data){
 		xScale.domain(
-			d3.extent(data, function(d) { return d[num_submits]; })
+			d3.extent(data, function(data) { return data[num_submits]; })
 			);
 		yScale.domain([
-			d3.min(data, function(d) { return d[id]; } ),
-			d3.max(data, function(d) { return d[id]; } )
+			d3.min(data, function(data) { return data[id] -0.5; } ),
+			d3.max(data, function(data) { return data[id] +0.5; } )
 			]);
 
 		xAxisG.call(xAxis);
 		yAxisG.call(yAxis);
 
-		var bar = graph.selectAll('rect').data(data);
+		var bar = g.selectAll('rect').data(data);
 
 		bar.enter().append('rect')
 			.attr('x',1)
-			.attr('y', function(d){ return yScale(d[id]); } )
+			.attr('y', function(data){ return yScale(data[id]) -8; } )
 			.attr('height', innerHeight/11)
-            .attr('width', function(d) { return xScale(d[num_submits]); })
+            .attr('width', function(data) { return xScale(data[num_submits]); })
             .attr('fill', 'orange' )
             .attr('padding',2);
+
+        bar.enter().append('text')
+        	.attr('x',-5)
+        	.attr('y', function(data){ return yScale(data[id]) +6;})
+        	.text(function(data){ return data[username]; } )
+        	.style('text-anchor','end');    
 	}
 })();
