4ef20a9fb871ef2c8a5a67b148d1063f52f7f776
Mikkel Hartmann Jensen <mikkelheart@gmail.com>
2018-10-24 19:29:31 +0200

    All the figures should now be fully responsive.

--- js/d3/lagtimes.js
+++ js/d3/lagtimes.js
@@ -1,15 +1,21 @@
 (function() {
-  (function() {
     d3.json("../../../../data/lagtimes.json", function(error, rawData) {
       for (var axesIdx = 0; axesIdx < rawData.lagtime.length; axesIdx++) {
         (function() {
-          var width = 300;
-          var height = 200;
-          var origin = [150, 100];
+          var windowWidth = parseInt(d3.select('body').style('width'), 10);
+          if (windowWidth > 600) {
+            width = 300;
+            height = 200;
+          } else {
+            width = 0.4 * windowWidth;
+            height = 0.66 * width;
+          }
+          
+          var origin = [width/2, height/2];
           var scale = 3;
           var trajectory = [];
           var mx, my, mouseX, mouseY;
-
+          
           // I want to have the figures in a grid.
           cell = {};
           if (axesIdx % 2 == 0) {
@@ -22,15 +28,19 @@
             cell.alignment = "right";
           }
 
+          // Create the div that holds the title and trajectory
           var div = cellDiv.append("div").classed(cell.alignment, true);
 
+          // Add the title to the div
           div
             .append("div")
             .append("p")
             .text(`lagtime: ${rawData.lagtime[axesIdx].value}`);
 
+          // Create the SVG that will hold the trajectory.
           var svg = div
             .append("svg")
+            .attr("class", "lagtimeSvg")
             .attr("width", width)
             .attr("height", height)
             .call(
@@ -42,6 +52,7 @@
             )
             .append("g");
 
+          // Create the trajectory
           var trajectory3d = d3
             ._3d()
             .shape("LINE_STRIP")
@@ -50,13 +61,17 @@
             .rotateX(-startAngle)
             .scale(scale);
 
-          function processData(data) {
+          // The function is used to draw the trajectory every time 
+          // the SVG is dragged or the window is resized.
+          function drawTrajectory(data) {
             var trajectoryLine = svg
               .selectAll("path.trajectory")
               .data(data.trajectory);
+            
             trajectoryLine
               .enter()
               .append("path")
+              .attr("class", 'lagtime')
               .attr("class", "_3d trajectory")
               .merge(trajectoryLine)
               .attr("stroke", lightColorUsed)
@@ -74,16 +89,19 @@
               parseFloat(d.z[i])
             ]);
           }
+
           var data = {
             trajectory: trajectory3d([trajectory])
           };
-          processData(data);
+
+          drawTrajectory(data);
 
           function dragStart() {
             mx = d3.event.x;
             my = d3.event.y;
           }
 
+          // Updates the data when the SVG is dragged.
           function dragged() {
             mouseX = mouseX || 0;
             mouseY = mouseY || 0;
@@ -94,15 +112,54 @@
                 .rotateY(beta + startAngle)
                 .rotateX(alpha - startAngle)([trajectory])
             };
-            processData(data);
+            drawTrajectory(data);
           }
 
           function dragEnd() {
             mouseX = d3.event.x - mx + mouseX;
             mouseY = d3.event.y - my + mouseY;
           }
+
+          function resizeChart() {
+            var windowWidth = parseInt(d3.select('body').style('width'), 10);
+            if (windowWidth > 600) {
+              width = 300;
+              height = 200;
+              scale = 3;
+            } else {
+              width = 0.4 * windowWidth;
+              height = 0.66 * width;
+              scale = 2;
+            }
+                        
+            d3.selectAll(".lagtimeSvg")
+              .attr("width", width)
+              .attr("height", height);
+
+            var origin = [width/2, height/2]
+
+            // Im creating a new 3d trajectory using the redefined scale and origin.
+            trajectory3d = d3
+                ._3d()
+                .shape("LINE_STRIP")
+                .origin(origin)
+                .rotateY(startAngle)
+                .rotateX(-startAngle)
+                .scale(scale);
+            
+            // I need to update the data so it contains the ned 3d trajectory.
+            var data = {
+              trajectory: trajectory3d
+                .rotateY(beta + startAngle)
+                .rotateX(alpha - startAngle)([trajectory])
+            };
+            
+            // Now I can redraw the data.
+            drawTrajectory(data);
+          }
+      
+          window.addEventListener('resize', resizeChart);
         })();
       }
-    });
-  })();
+    });    
 })();
--- js/d3/ln-c-ln-epsilon.js
+++ js/d3/ln-c-ln-epsilon.js
@@ -1,77 +1,139 @@
 (function() {
-  d3.csv("../../../../data/ln_c_ln_epsilon.csv", function(data) {
-    var margin = { top: 20, right: 20, bottom: 40, left: 45 };
-    var height = 300;
-    var width = 600;
+    d3.csv("../../../../data/ln_c_ln_epsilon.csv", function(data) {
+      var margin = { top: 20, right: 20, bottom: 40, left: 45 };
+      var windowWidth = parseInt(d3.select('body').style('width'), 10) - margin.left - margin.right;
+      
+      if (windowWidth > 600) {
+        width = 600;
+        height = 300;
+      } else {
+        width = windowWidth;
+        height = 0.5 * windowWidth;
+      }
+  
+      var xScale = d3
+        .scaleLinear()
+        .domain(d3.extent(data, d => parseFloat(Math.log(d.epsilon))))
+        .range([margin.left, width - margin.right]);
+  
+      var yScale = d3
+        .scaleLinear()
+        .domain(d3.extent(data, d => parseFloat(Math.log(d.C))))
+        .range([height - margin.bottom, margin.top]);
+  
+      var xAxis = d3
+        .axisBottom()
+        .scale(xScale)
+        .ticks(5);
+      var yAxis = d3
+        .axisLeft()
+        .scale(yScale)
+        .ticks(5);
+  
+      var svg = d3
+        .select("#ln_c_ln_epsilon")
+        .attr(
+          "style",
+          `padding-bottom: ${Math.ceil(height * 100 / width)}%`
+        )
+        .append("svg")
+        .attr("height", height)
+        .attr("width", width)
+  
+      svg
+        .append('g')      
+        .selectAll("circle")
+        .data(data)
+        .enter()
+        .append("circle")
+        .attr("cx", d => xScale(Math.log(d.epsilon)))
+        .attr("cy", d => yScale(Math.log(d.C)))
+        .attr("r", 2)
+        .attr("fill", darkColorUsed);
+  
+      var yAxisEl = svg
+        .append("g")
+        .attr("class", "axis")
+        .attr("transform", `translate(${margin.left},0)`)
+        .call(yAxis);
+  
+      var xAxisEl = svg
+        .append("g")
+        .attr("class", "axis")
+        .attr("transform", `translate(0, ${height - margin.bottom})`)
+        .call(xAxis);
+  
+      var xAxisLabel =svg
+        .append("text")
+        .attr("class", "axisLabel")
+        .text("log Epsilon")
+        .attr("x", width / 2)
+        .attr("y", height - 5)
+        .style("text-anchor", "middle");
+  
+      var yAxisLabel = svg
+        .append("text")
+        .attr("class", "axisLabel")
+        .text("log C")
+        .attr("transform", "rotate(-90)")
+        .attr("y", 15)
+        .attr("x", -height / 2)
+        .style("text-anchor", "middle");
 
-    var xScale = d3
-      .scaleLinear()
-      .domain(d3.extent(data, d => parseFloat(Math.log(d.epsilon))))
-      .range([margin.left, width - margin.right]);
+      function resizeChart() {
+        var windowWidth = parseInt(d3.select('body').style('width'), 10) - margin.left - margin.right;
 
-    var yScale = d3
-      .scaleLinear()
-      .domain(d3.extent(data, d => parseFloat(Math.log(d.C))))
-      .range([height - margin.bottom, margin.top]);
+        if (windowWidth > 600) {
+          width = 600;
+          height = 300;
+        } else {
+          width = windowWidth;
+          height = 0.5 * windowWidth;
+        }
+        
+        svg
+          .attr("height", height)
+          .attr("width", width)
+  
+        
+        xScale.range([margin.left, width - margin.right]);
+        xAxis.scale(xScale);
+        xAxisEl
+          .attr("transform", `translate(0, ${height - margin.bottom})`)
+          .call(xAxis);
 
-    var xAxis = d3
-      .axisBottom()
-      .scale(xScale)
-      .ticks(5);
-    var yAxis = d3
-      .axisLeft()
-      .scale(yScale)
-      .ticks(5);
+        yScale.range([height - margin.bottom, margin.top]);
+        yAxis.scale(yScale);
+        yAxisEl
+          .attr("transform", `translate(${margin.left},0)`)
+          .call(yAxis);
 
-    var svg = d3
-      .select("#ln_c_ln_epsilon")
-      .attr(
-        "style",
-        `padding-bottom: ${Math.ceil(height * 100 / width)}%`
-      )
-      .append("svg")
-      .attr("preserveAspectRatio", "xMinYMin meet")
-      .attr("ViewBox", `0 0 ${width} ${height}`)
-      .classed("svg-content-responsive", true)
+        xAxisLabel
+          .attr("x", width / 2)
+          .attr("y", height - 5)
 
-    svg
-      .append('g')      
-      .selectAll("circle")
-      .data(data)
-      .enter()
-      .append("circle")
-      .attr("cx", d => xScale(Math.log(d.epsilon)))
-      .attr("cy", d => yScale(Math.log(d.C)))
-      .attr("r", 2)
-      .attr("fill", darkColorUsed);
+         yAxisLabel
+          .attr("y", 15)
+          .attr("x", -height / 2)
+        
+        // Here I an removing the circles and redrawing them. It really 
+        // Shoud be possible to simply change the data.
+        d3.selectAll("circle").remove();
+        
+        svg
+          .append('g')      
+          .selectAll("circle")
+          .data(data)
+          .enter()
+          .append("circle")
+          .attr("cx", d => xScale(Math.log(d.epsilon)))
+          .attr("cy", d => yScale(Math.log(d.C)))
+          .attr("r", 2)
+          .attr("fill", darkColorUsed);
+      }
+    
+      // redraw chart on resize
+      window.addEventListener('resize', resizeChart);
 
-    svg
-      .append("g")
-      .attr("class", "axis")
-      .attr("transform", `translate(${margin.left},0)`)
-      .call(yAxis);
-
-    svg
-      .append("g")
-      .attr("class", "axis")
-      .attr("transform", `translate(0, ${height - margin.bottom})`)
-      .call(xAxis);
-
-    svg
-      .append("text")
-      .attr("class", "axisLabel")
-      .text("log Epsilon")
-      .attr("x", width / 2)
-      .attr("y", height - 5)
-      .style("text-anchor", "middle");
-
-    svg
-      .append("text")
-      .attr("class", "axisLabel")
-      .text("log C")
-      .attr("transform", "rotate(-90)")
-      .attr("y", 15)
-      .attr("x", -height / 2)
-      .style("text-anchor", "middle");
-  });
+    });
 })();
